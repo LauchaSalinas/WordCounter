a6791dbcc4b275abb26b90507ce6d3a3d91fe88a
Laucha Salinas <salinas.lautarod@gmail.com>
2023-12-13 23:40:12 -0300

    Add project files.

---
+++ WordCounter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WordCounter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ WordCounter/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WordCounter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
